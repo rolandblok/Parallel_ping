1cc945c055c4fe3d91f5d34bfc31af2f00cc4cfe
roland <rolandblok@gmail.com>
2019-06-09 16:35:58 +0200

    summary and host resolve

--- IPscan_parallel.py
+++ IPscan_parallel.py
@@ -11,6 +11,7 @@ full_response = {}
 results = {}
 
 
+
 class PingerThread (threading.Thread):
    def __init__(self, scan, ip_adress):
       threading.Thread.__init__(self)
@@ -18,26 +19,34 @@ class PingerThread (threading.Thread):
       self.ip_adress = ip_adress
    def run(self):
       #Ping the ip address
-      ping_response = subprocess.Popen(["ping", self.ip_adress, "-n", '1'],   stdout=subprocess.PIPE).stdout.read()
+      ping_response = subprocess.Popen(["ping", "-a", self.ip_adress, "-n", '1'],   stdout=subprocess.PIPE).stdout.read()
       ping_response = ping_response.decode()
       full_response[self.scan] = ping_response
       stat = ""
-      if ("unreachable"  in ping_response) or ("timed out"  in ping_response) :
-          stat = "" + self.ip_adress + " does NOT respond"
+      if ("unreachable"  in ping_response) :
+         stat = "" + self.ip_adress + " unreachable"
+      elif ("timed out"  in ping_response) : 
+         stat = "" + self.ip_adress + " timed out"
       else :
-          stat = "" + self.ip_adress + " alive"
+         # host resolve
+         host = "unknown"
+         start_host_str = int(ping_response.find("Pinging")) + 8
+         end_host_str   = int(ping_response.find("["+hostname)) - 1
+         if ( ( start_host_str > 0 ) and (end_host_str > 0) ) :
+            host = ping_response[start_host_str:end_host_str]            
+         stat = "" + self.ip_adress + " [" + host + "] alive"
       results[self.scan] = stat
 
 
 threads = {}
-scans = range(1, 254)
+scans = range(1, 255)
 for scan in scans :
    ip_address = str(hostname) + "." + str(scan)
    threads[scan] = PingerThread(scan, ip_address)
-   threads[scan].start();
+   threads[scan].start()
 
 for scan in scans :    
-    threads[scan].join();
+   threads[scan].join()
 
     
 with open(str(file_name), "w")as file:
@@ -45,8 +54,10 @@ with open(str(file_name), "w")as file:
       print(results[scan])
       file.write(results[scan]+"\n")
    for scan in scans :    
-      print(results[scan])
       file.write(full_response[scan]+"\n")
    
-   
-   
+print("\n========summary========\n")
+for scan in scans :   
+   if ( results[scan].find("alive")  > 0 ) :
+      print(results[scan])
+print("\n")
